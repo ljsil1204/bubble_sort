f3ed2c47999425b2845f3f4ff1fed51b380a24dd
lee jin sil <ljsil1204@naver.com>
2026-03-03 11:39:39 +0900

    버블정렬 프로그램
    공부용 주석 추가

--- main.c
+++ main.c
@@ -6,11 +6,27 @@
 int list[MAX_SIZE];
 int n;
 
-// 버블정렬(Bubble Sort) 
+// 버블정렬(Bubble Sort) => 이웃한 항을 비교하여 정렬하는 알고리즘
 void bubble_sort(int list[], int n) {
 
-	
+	int i, j, temp;
 
+	// i의 범위 : 1 ~ n-1(배열의 마지막 인덱스)
+	// 버블정렬은 맨 자리부터 정렬되므로 i의 인덱스 순회를 뒤에서 부터 진행한다.
+	for (i = n - 1; i > 0; i--) {
+
+		// 하나의 회차 동안에 인접하는 요소를 비교하는 반복문
+		// i=9, j = 0 일 경우, list[0] > list[1]를 비교하여 값을 교환한다.
+		// i=9, j = 1 일 경우, list[1] > list[2]를 비교하여 값을 교환한다.
+		// i=9, j = 8 일 경우, list[8] > list[9]를 비교하여 값을 교환한다. 
+		// => j<i인 이유로, j는 마지막인덱스(n-1)이나 이미 정렬된 영역보다 작아야 한다.
+		for (j = 0; j < i; j++) {
+			if (list[j] > list[j + 1]) {
+				SWAP(list[j], list[j + 1], temp);
+			}
+		}
+
+	}
 }
 
 int main(void) {
